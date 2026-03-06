5918defeae31625eecb68a8439daac7f22247a75
Ashita_02 <ashitavarshney37532@gmail.com>
2026-03-06 22:05:00 +0530

    [Ashita] Add . Implementation of UC6 Adding Length Units

---
+++ QuantityMeasurementApp/Exceptions/QuantityMeasurementException.cs
@@ -0,0 +1,10 @@
+namespace QuantityMeasurementApp.Exceptions
+{
+    public class QuantityMeasurementException : Exception
+    {
+        public QuantityMeasurementException(string message)
+            : base(message)
+        {
+        }
+    }
+}
